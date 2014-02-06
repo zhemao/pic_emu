2a56ba75c37ef608047097b02435df4c0e501b50
Howard Mao <zhehao.mao@gmail.com>
2014-02-05 22:06:41 -0800

    fix bank offset

--- memory.go
+++ memory.go
@@ -1,11 +1,11 @@
 package main
 
 func getRegValue(state *emuState, addr uint16) byte {
-    full_addr := uint16(4 * state.bank) + addr
+    full_addr := uint16(state.bank << RAM_BANK_SHIFT) | addr
     return state.data_ram[full_addr]
 }
 
 func setRegValue(state *emuState, addr uint16, value byte) {
-    full_addr := uint16(4 * state.bank) + addr
+    full_addr := uint16(state.bank << RAM_BANK_SHIFT) | addr
     state.data_ram[full_addr] = value
 }
