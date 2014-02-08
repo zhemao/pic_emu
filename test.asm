09ccb9094e8415994062f9df5bb2942bed7677ff
Howard Mao <zhehao.mao@gmail.com>
2014-02-07 20:38:07 -0800

    more extensive test program

--- test.asm
+++ test.asm
@@ -1,22 +1,41 @@
 list p=16f74
 title "add test"
 
+#include <p16f74.inc>
+
   org 00h
   goto main
 
   org 04h
-  goto isrService
+  goto errout
 
   org 05h
 
 main
+  clrw ; w = 0, Z is set
   movlw 7 ; w = 7
-  addlw 5 ; w = 12
+  addlw 5 ; w = 12, Z is unset
+  btfsc STATUS, Z
+  goto errout
   movwf 21h
   movlw 5 ; w = 5
-  addwf 21h, w ; w = 17
+  addwf 21h, w ; w = 17, DC is set
+  btfss STATUS, DC
+  goto errout
+  sublw 11h
+  btfss STATUS, Z
+  goto errout
+  movlw 3
+  movwf 21h
+  bcf STATUS, C
+  bcf STATUS, Z
+  rrf 21h, f
+  rlf 21h, f
+  sublw 3
+  btfss STATUS, Z
+  goto errout
+  retlw 0
 
-isrService
-  goto isrService
-
+errout
+  retlw 1
   end
